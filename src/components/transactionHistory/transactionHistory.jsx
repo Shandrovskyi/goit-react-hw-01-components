da9292a835897d68086710a415ff76ab00765a02
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-06-27 15:45:17 +0300

    Update transactionHistory.jsx

--- src/components/transactionHistory/transactionHistory.jsx
+++ src/components/transactionHistory/transactionHistory.jsx
@@ -26,17 +26,15 @@ export function TransactionHistory({ items }) {
   );
 }
 
-
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        amount: PropTypes.string.isRequired,
-        currency: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-  };
-  
-  export default TransactionHistory;
-  
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
