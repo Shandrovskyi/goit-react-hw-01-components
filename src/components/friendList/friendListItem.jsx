106b653a661b69bdf94971fcfaf540713f9ed5c5
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-06-27 15:45:30 +0300

    Update friendListItem.jsx

--- src/components/friendList/friendListItem.jsx
+++ src/components/friendList/friendListItem.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import css from './friendList.module.css';
+import PropTypes from 'prop-types';
 
 export function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={css.item}>
-      <span  className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
+      <span className={isOnline ? css.online : css.offline} />
       <img className={css.avatar} src={avatar} alt="User avatar" />
       <p className={css.name}>{name}</p>
     </li>
   );
 }
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  avatar: PropTypes.number.isRequired,
+};
