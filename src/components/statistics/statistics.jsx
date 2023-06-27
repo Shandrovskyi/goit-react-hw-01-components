9d404090d34c4c98207262ebb27a114b903d9636
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-06-27 15:45:00 +0300

    Update statistics.jsx

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -1,31 +1,46 @@
-import css from './statistics.module.css'
+import css from './statistics.module.css';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-export function Statistics ({title, stats}) {
-return(
+export function Statistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-<section className={css.statistics}>
-<h2 className={css.title}>{title}</h2>
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li
+            key={stat.id}
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={css.label}>{stat.label}&nbsp;</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
-  <ul className={css.statList}>
-    <li className={css.item}>
-      <span className={css.label}>{stats[0].label}&nbsp;</span>
-      <span className={css.percentage}>32%</span>
-    </li>
-    <li class={css.item}>
-      <span className={css.label}>{stats[1].label}&nbsp;</span>
-      <span className={css.percentage}>4%</span>
-    </li>
-    <li class={css.item}>
-      <span className={css.label}>{stats[2].label}&nbsp;</span>
-      <span className={css.percentage}>17%</span>
-    </li>
-    <li className={css.item}>
-      <span className={css.label}>{stats[3].label}&nbsp;</span>
-      <span className={css.percentage}>47%</span>
-    </li>
-  </ul>
-</section>
+// Підскажіть, такі функції потрібно виносити в інші файли чи все залежить від кількості і розміру ??
 
-);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+// =======================================================================================================
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
